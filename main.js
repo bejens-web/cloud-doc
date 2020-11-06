a0a8d92362b95f321f42c38d2b1ed69e79aab582
xu_wei <1012078595@qq.com>
2020-11-06 10:11:30 +0800

    init

--- main.js
+++ main.js
@@ -3,15 +3,14 @@ const isDev = require("electron-is-dev");
 let mainWindow = null;
 
 
-
 const createWindow = async () => {
     //隐藏默认导航
     Menu.setApplicationMenu(null);
     mainWindow = new BrowserWindow({
         width: 1024,
         height: 680,
-        minWidth:1024,
-        minHeight:680,
+        minWidth: 1024,
+        minHeight: 680,
         frame: true, //window自带的关闭最小化等
         resizable: true, //改变主窗口尺寸
         webPreferences: {
@@ -27,12 +26,9 @@ const createWindow = async () => {
 
     await mainWindow.loadURL(urlLocation);
 
-
     mainWindow.on('closed', () => {
         mainWindow = null;
     });
-
-
 };
 
 app.on('ready', createWindow);
